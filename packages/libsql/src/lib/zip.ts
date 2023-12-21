b103b0b5b1d9f817c005dacf14af6449eface070
Aleks Rūtiņš <aleks@rutins.com>
2023-12-21 14:13:43 -0500

    Add lint

--- packages/libsql/src/lib/zip.ts
+++ packages/libsql/src/lib/zip.ts
@@ -1,3 +1,3 @@
-export function zip<A1 extends any[], A2 extends any[]>(a1: A1, a2: A2) {
+export function zip<A1 extends unknown[], A2 extends unknown[]>(a1: A1, a2: A2) {
     return a1.map((key, idx) => [key, a2[idx]]);
 }
